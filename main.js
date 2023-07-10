10c18ef6caf3dd2927d95e968958c509b25227e2
Gabriel-Paulino12 <127151177+Gabriel-Paulino12@users.noreply.github.com>
2023-07-10 17:59:41 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ var array=[];
 function EnviarNomes(){
     array.push(document.getElementById("nome").value)
     console.log(array);
-    //document.getElementById("nome").innerHTML.value="B ";
     document.getElementById("Mostrarnome").innerHTML=array;
+     //document.getElementById("nome").value=" ";
 }
 function mostrarnomes(){
     var name="";
@@ -11,28 +11,27 @@ function mostrarnomes(){
     name=name+array[i]+"<br>" 
     }
     console.log(name);
-    document.getElementById("nomesmostrados").innerHTML=array;
+    document.getElementById("nomesmostrados").innerHTML=name;
 }
 function submit(){
     array.sort();
     var listaNomesFinalOrdenada=[];
-    for(var k=0; k<array.length; k++)
-    {
-        listaNomesFinalOrdenada.push("<h4>NOME - "+ array[k]+"</h4>");
-    }
-    var listaNomesFinal=listaNomesFinalOrdenada.join(" ");
-    document.getElementById("Ordenarnomes").innerHTML=array;
+    for(i=0; i<array.length; i++){
+        listaNomesFinalOrdenada=listaNomesFinalOrdenada+array[i]+"<br>" 
+        }
+        console.log(listaNomesFinalOrdenada);
+    document.getElementById("Ordenarnomes").innerHTML=listaNomesFinalOrdenada;
 }
 function search()
 {
-    var s=document.getElementById("s1").value;
+    var s=document.getElementById("pesquisa").value;
     var found=0;
     var j;
-    for(j=0; j<nome.length; j++){
-        if(s==nome[j]){
+    for(j=0; j<array.length; j++){
+        if(s==array[j]){
             found=found+1;
         }
      }
-     document.getElementById("p2").innerHTML="pesquisar "+found+" vez(es)";
+     document.getElementById("p2").innerHTML=s +" "+found+" vez(es)";
      console.log("pesquisar "+found+" vez(es)");
 }
